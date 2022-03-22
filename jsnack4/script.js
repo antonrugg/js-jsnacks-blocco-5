c33e87f3868e9e994325c081b1b1f1e42db3ed53
Antonio Ruggiero <antonrugg@gmail.com>
2022-03-22 16:49:33 +0100

    js snack 4 bonus

--- jsnack4/script.js
+++ jsnack4/script.js
@@ -1,2 +1,20 @@
 // Scrivi una funzione che fonda due array(aventi lo stesso numero di elementi)
 // prendendo alternativamente gli elementi da uno e dall’altro, es. [a, b, c], [1, 2, 3] →[a, 1, b, 2, c, 3].
+
+
+const arrayLettere = ['a', 'b', 'c'];
+const arrayNumeri = ['1', '2', '3'];
+const arrayFuso = [];
+// const arrayFuso = [...arrayLettere, ...arrayNumeri];
+
+function joinArray() {
+    for (let i = 0; i < arrayLettere.length; i++){
+        arrayFuso.push(arrayLettere[i]);
+        arrayFuso.push(arrayNumeri[i]);
+    }
+        
+}
+
+
+joinArray();
+console.log(arrayFuso);
