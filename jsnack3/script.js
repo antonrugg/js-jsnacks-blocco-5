85dfe7eff2c4c45e60d01e04031cb82d46c4ba42
Antonio Ruggiero <antonrugg@gmail.com>
2022-03-22 15:56:47 +0100

    js snack 3

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,1 +1,19 @@
 // Scrivi una funzione che accetti una stringa come argomento e la ritorni girata(es.Ciao -> oaiC)
+
+
+// function reverseString(str) {
+//     return str.split("").reverse().join("");
+// }
+// reverseString('ciao');
+
+// console.log(reverseString('ciao'));
+
+
+
+const reverseString = (str) => {
+    return str.split('').reverse().join('');
+}
+
+reverseString('ciao');
+
+console.log(reverseString('ciao'));
